da7a9b378ee1adc63ba1813621f4d25979e39a4f
Doooooooooooo λλλλλλ <90887415+DoTruong1@users.noreply.github.com>
2024-05-18 01:32:57 +0700

    add: orm, connect to db, example ,env (#1)
    Co-authored-by: Trương Độ <dotruong@dotruong.local>

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express')
-require('dotenv').config()
+const cors = require("cors");
 const app = express()
+const database = require("./sequelize")
+const models = require("./sequelize/models")
+require('dotenv').config()
+
+var corsOptions = {
+  origin: process.env.COR_ORIGIN
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const initApp = async () => {
+  console.log("Kiểm tra kết nối đến cơ sở dữ liệu..");
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  // Test the connection.
+  // You can use the .authenticate() function to test if the connection works.
+
+  try {
+    await database.authenticate();
+    console.log("Kết nối đến cơ sở dữ liệu thành công.");
+
+    // Syncronize the Book model.
+    models.User.sync({ alter: true });
+    console.log("Kết nối đến cơ sở dữ liệu thành công.");
+
+    // Start the web server on the specified port.
+    app.listen(port, () => {
+      console.log(`Khởi tạo server ở: http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Có lỗi khi kết nối đến cơ sở dữ liệu:", error.original);
+  }
+};
+
+
+
+initApp()
--- sequelize/index.js
+++ sequelize/index.js
@@ -3,8 +3,11 @@ const { Sequelize } = require('sequelize');
 
 const config = require('../config/db.config')
 
+console.log(config.database)
 const sequelize = new Sequelize(config.database.database_name, config.database.username, config.database.password, {
   host: config.database.host,
-  port: config.database.port,
+  port: 3306,
   dialect: config.database.dialect /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
+
+module.exports = sequelize;
