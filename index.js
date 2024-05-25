b30ab4aee99c2e25c41729874f710c3fbdb392ec
Doooooooooooo λλλλλλ <90887415+DoTruong1@users.noreply.github.com>
2024-05-26 03:40:08 +0700

    Feature/add cache repo (#26)
    * update README
    
    * update README
    
    * update README
    
    * Update/readme (#24)
    
    * test trigger
    
    * test trigger
    
    ---------
    
    Co-authored-by: Trương Độ <dotruong@dotruong.local>
    
    * Test pipe
    
    * save
    
    * Apple is shit
    
    * Test pipe
    
    * Test cache
    
    * .DS_Store fu!
    
    * test if cache is working
    
    * .DS_Store fu!
    
    ---------
    
    Co-authored-by: Trương Độ <dotruong@dotruong.local>

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 // var corsOptions = {
 //   origin: process.env.COR_ORIGIN,
 // };
-// test cache
+// test cache baby
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
