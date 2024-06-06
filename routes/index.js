874d18a00b5a7581b937cd1a003dfcf96872055e
Doooooooooooo λλλλλλ <90887415+DoTruong1@users.noreply.github.com>
2024-06-06 16:32:49 +0700

    Feature/add health check route (#28)
    * .DS_Store fu!
    
    * update sequelize
    
    * fix field
    
    * add healthcheck, run docker build and push on tag
    
    * add healthcheck, update helm chart, run docker build and push on tag
    
    ---------
    
    Co-authored-by: Trương Độ <dotruong@dotruong.local>
    Co-authored-by: Độ Trương <dotruong@MacBook-Pro-cua-o.local>

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,12 @@
 var routers = require("express").Router();
 const userRouters = require("./user.route");
-
+const heatlthCheckRouters = require("./healthCheck.route")
 /* GET home page. */
 // router.get('/', function (req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
 // console.log("in routes")
 routers.use('/users', userRouters)
-
+// routers.use('/healthz',heatlthCheckRouters)
 module.exports = routers;
 
-// const router = require('express').Router();
-// const apiRoutes = require('./api-routes');
-// const htmlRoutes = require('./html-routes');
-
-// router.use('/api', apiRoutes);
-
-// router.use('/', htmlRoutes);
-
-// module.exports = router;
