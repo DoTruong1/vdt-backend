1241368271ce3eddfabadbaab53c9f9883bec071
Độ Trương <dotruong@MacBook-Pro-cua-o.local>
2024-06-07 22:33:20 +0700

    add prometheus, healthcheck

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const promMid = require('express-prometheus-middleware');
 const app = express();
 const db_conn = require("./config/connect.database");
 const heatlthCheckRouters = require("./routes/healthCheck.route")
@@ -28,21 +29,42 @@ const initApp = async () => {
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-
-    heatlthCheckRouters.get('/healthz', async (_req, res, _next) => {
-
-      const healthcheck = {
-          uptime: process.uptime(),
-          message: 'OK',
-          timestamp: Date.now()
-      };
-      try {
-          res.send(healthcheck);
-      } catch (error) {
-          healthcheck.message = error;
-          res.status(503).send();
-      }
-    });
+    
+    app.use(promMid({
+      metricsPath: '/metrics',
+      collectDefaultMetrics: true,
+      requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+      requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+      responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+      prefix: 'vdt_backend_'
+      /**
+       * Uncomenting the `authenticate` callback will make the `metricsPath` route
+       * require authentication. This authentication callback can make a simple
+       * basic auth test, or even query a remote server to validate access.
+       * To access /metrics you could do:
+       * curl -X GET user:password@localhost:9091/metrics
+       */
+      // authenticate: req => req.headers.authorization === 'Basic dXNlcjpwYXNzd29yZA==',
+      /**
+       * Uncommenting the `extraMasks` config will use the list of regexes to
+       * reformat URL path names and replace the values found with a placeholder value
+      */
+      // extraMasks: [/..:..:..:..:..:../],
+      /**
+       * The prefix option will cause all metrics to have the given prefix.
+       * E.g.: `app_prefix_http_requests_total`
+       */
+      // prefix: 'app_prefix_',
+      /**
+       * Can add custom labels with customLabels and transformLabels options
+       */
+      // customLabels: ['contentType'],
+      // transformLabels(labels, req) {
+      //   // eslint-disable-next-line no-param-reassign
+      //   labels.contentType = req.headers['content-type'];
+      // },
+    }));
+    app.use('/healthz', heatlthCheckRouters)
 
     app.use(process.env.API_PATH, routers);
 
